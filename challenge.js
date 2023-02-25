856c8e3c9a6679594120e63c06318b2536e2ed10
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-02-25 06:12:12 +0430

    initail solution

--- challenge.js
+++ challenge.js
@@ -1,12 +1,46 @@
 // Start with your code from LinkedList challenge.
-
-class Queue {
-  add(number) {
-    // your code here
+// Linked List
+class Node {
+  constructor(data) {
+    this.data = data
+    this.next = null
   }
-  
+}
+class Queue {
+  constructor() {
+    this.first = null
+    this.last = null
+    this.size = 0
+  }
+
+  isEmpty() {
+    return !this.size
+  }
+
+
+  add(item) {
+    const node = new Node(item)
+    if (this.isEmpty()) {
+      this.first = node
+      this.last = node
+    }
+    else {
+      this.last.next = node
+      this.last = node
+    }
+    this.size++
+    return this
+  }
+
   remove() {
-    // your code here
+    if (this.isEmpty()) return -1
+    const remove = this.first
+    if (this.first === this.last) {
+      this.last = null
+    }
+    this.first = this.first.next
+    this.size--
+    return remove.data
   }
 }
 
